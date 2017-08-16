1b208f92a1f5d1ee22b6ece1c0301cb7a39cd60a
Jan de Graaf <jdegraaffoto@xs4all.nl>
2017-08-16 17:41:45 +0200

    diversen

--- gulpfile.js
+++ gulpfile.js
@@ -41,18 +41,19 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
-gulp.task('sass', function () {
-    return gulp.src('_scss/main.scss')
-        .pipe(sass({
-            includePaths: ['scss'],
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
-});
+ gulp.task('sass', function () {
+     return gulp.src('assets/css/main.scss')
+         .pipe(sass({
+             includePaths: ['css'],
+             onError: browserSync.notify
+         }))
+         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+         .pipe(gulp.dest('_site/assets/css'))
+         .pipe(browserSync.reload({stream:true}))
+         .pipe(gulp.dest('assets/css'));
+ });
 
+ 
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
